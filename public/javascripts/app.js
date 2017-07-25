e2da75ba991402633ce01ae34d0add061f75af0e
johnson-rl <johnson.rl@gmail.com>
2017-07-25 11:03:48 -0700

    video selector working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -134,6 +134,94 @@ jQuery(function($) {
     previousTime = 0,
     activePgn
 
+    let videoList = {
+      3: {
+        title: '103_GK_Double Attacks_Board_v7',
+        hash: 'eu2yodfadf',
+        chapter: 3
+      },
+      4: {
+        title: '104_GK_Skewers_Board_v7',
+        hash: '8xwnkkqkxc',
+        chapter: 4
+      },
+      5: {
+        title: '105_GK_DiscoveredAttacks_Board_v7',
+        hash: 'vsjsugodzx',
+        chapter: 5
+      },
+      6: {
+        title: '106_GK_Pins_Board_v7',
+        hash: 'vuux3jpt5i',
+        chapter: 6
+      },
+      7: {
+        title: '107_GK_DeflectionAttraction_Board_v7',
+        hash: 'uv3rk57e8r',
+        chapter: 7
+      },
+      8: {
+        title: '108_GK_Interference_Board_v7',
+        hash: 'rkh884dbuh',
+        chapter: 8
+      },
+      9: {
+        title: '109_GK_Overload_Board_v7',
+        hash: 'otunyj55s8',
+        chapter: 9
+      },
+      10: {
+        title: '110_GK_WinningTrades_Board_v7',
+        hash: 'anz4s14b4a',
+        chapter: 10
+      },
+      11: {
+        title: '111_GK_EndGames_Board_v7',
+        hash: 'wc23s0sw6y',
+        chapter: 11
+      },
+      12: {
+        title: '112_GK_Openings_Board_v7',
+        hash: '8g4xna0a0g',
+        chapter: 12
+      },
+      14: {
+        title: '114_GK_Case Studies_Board_Openings_v4',
+        hash: 'fjgr7q5hp4',
+        chapter: 14
+      },
+      16: {
+        title: '116_GK_Case Studies_EndGames_Board_v6',
+        hash: 'pb54t3rwan',
+        chapter: 16
+      },
+      22: {
+        title: '122_GK_BonusSecretNovelty_Board_v6',
+        hash: 'p0c2jxbx2w',
+        chapter: 122
+      // },
+      // 13: {
+      //   title: '113_GK_Simul_Board_v4',
+      //   hash: '9nfd08l947',
+      //   chapter: 113
+      // },
+      // {
+      //   title: '113A_GK_Simul_Board_v4',
+      //   hash: 'qtvjty3g1n',
+      //   chapter: 113
+      // },
+      // {
+      //   title: '113B_GK_Simul_Board_v4',
+      //   hash: 'bq81ef0itb',
+      //   chapter: 113
+      // },
+      // {
+      //   title: '113C_GK_Simul_Board_v4',
+      //   hash: 'vmwh8lnf1z',
+      //   chapter: 113
+      }
+    }
+
   // $.ajax({
   //   method: 'GET',
   //   url: 'api/pgns',
@@ -158,17 +246,28 @@ jQuery(function($) {
     success: (data) => {
       newEvents = data.sort((a, b)=>{return a.timestamp - b.timestamp})
       console.log(newEvents)
-      loadVideo()
+      loadVideo('eu2yodfadf')
     }
   })
 
+  Object.values(videoList).forEach((video)=>{
+    $('#select-video').append(`<option value="${video.chapter}">${video.title}</option>`)
+  })
 
-  function storeVideos(data){
-    console.log(data)
-    data.forEach((video)=>{
-      $('#select-video').append(`<option value="${video.videoHash}">${video.title}</option>`)
+  $('#select-video').change(()=>{
+    let selected = $('#select-video option:selected').val()
+    console.log(selected)
+    $.ajax({
+      type: 'GET',
+      url: `/api/chapter/${selected}`,
+      success: (data) => {
+        newEvents = data.sort((a, b)=>{return a.timestamp - b.timestamp})
+        console.log(newEvents)
+        $('.video').replaceWith(`<div class="wistia_embed wistia_async_${videoList[selected].hash} video" style="width:701px;height:392px;"></div>`)
+        loadVideo(videoList[selected].hash)
+      }
     })
-  }
+  })
 
   function addEvent(pgn, data){
     $.ajax({
@@ -250,8 +349,8 @@ jQuery(function($) {
     previousTime = t
   }
 
-  function loadVideo(){
-    _wq.push({ id: 'eu2yodfadf', onReady: function(video) {
+  function loadVideo(hash){
+    _wq.push({ id: hash, onReady: function(video) {
       // console.log('videoData',videoData)
       //   loadMoveIntoEditor(result[0])
       // orderedEvents = videoData.events.sort((a, b)=>{return a.timestamp - b.timestamp})
--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -3,71 +3,88 @@ $(document).ready(()=>{
   let videoList = [
     {
       title: '103_GK_Double Attacks_Board_v7',
-      hash: 'eu2yodfadf'
+      hash: 'eu2yodfadf',
+      chapter: 3
     },
     {
       title: '104_GK_Skewers_Board_v7',
-      hash: '8xwnkkqkxc'
+      hash: '8xwnkkqkxc',
+      chapter: 4
     },
     {
       title: '105_GK_DiscoveredAttacks_Board_v7',
-      hash: 'vsjsugodzx'
+      hash: 'vsjsugodzx',
+      chapter: 5
     },
     {
       title: '106_GK_Pins_Board_v7',
-      hash: 'vuux3jpt5i'
+      hash: 'vuux3jpt5i',
+      chapter: 6
     },
     {
       title: '107_GK_DeflectionAttraction_Board_v7',
-      hash: 'uv3rk57e8r'
+      hash: 'uv3rk57e8r',
+      chapter: 7
     },
     {
       title: '108_GK_Interference_Board_v7',
-      hash: 'rkh884dbuh'
+      hash: 'rkh884dbuh',
+      chapter: 8
     },
     {
       title: '109_GK_Overload_Board_v7',
-      hash: 'otunyj55s8'
+      hash: 'otunyj55s8',
+      chapter: 9
     },
     {
       title: '110_GK_WinningTrades_Board_v7',
-      hash: 'anz4s14b4a'
+      hash: 'anz4s14b4a',
+      chapter: 10
     },
     {
       title: '111_GK_EndGames_Board_v7',
-      hash: 'wc23s0sw6y'
+      hash: 'wc23s0sw6y',
+      chapter: 11
     },
     {
       title: '112_GK_Openings_Board_v7',
-      hash: '8g4xna0a0g'
+      hash: '8g4xna0a0g',
+      chapter: 12
     },
     {
       title: '114_GK_Case Studies_Board_Openings_v4',
-      hash: 'fjgr7q5hp4'
+      hash: 'fjgr7q5hp4',
+      chapter: 14
     },
     {
       title: '116_GK_Case Studies_EndGames_Board_v6',
-      hash: 'pb54t3rwan'
+      hash: 'pb54t3rwan',
+      chapter: 16
     },
     {
       title: '122_GK_BonusSecretNovelty_Board_v6',
-      hash: 'p0c2jxbx2w'
+      hash: 'p0c2jxbx2w',
+      chapter: 122
     },
     {
       title: '113_GK_Simul_Board_v4',
-      hash: '9nfd08l947'
+      hash: '9nfd08l947',
+      chapter: 113
     },
     {
       title: '113A_GK_Simul_Board_v4',
-      hash: 'qtvjty3g1n'
+      hash: 'qtvjty3g1n',
+      chapter: 113
     },
     {
       title: '113B_GK_Simul_Board_v4',
-      hash: 'bq81ef0itb'
+      hash: 'bq81ef0itb',
+      chapter: 113
     },
     {
       title: '113C_GK_Simul_Board_v4',
-      hash: 'vmwh8lnf1z'
+      hash: 'vmwh8lnf1z',
+      chapter: 113
     }
   ]
 
